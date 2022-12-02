b84e872c0153c40499b5b7000263febfd2301f4c
pindi-try-hard <118323132+pindi-try-hard@users.noreply.github.com>
2022-12-02 07:11:13 +0530

    Add files via upload

--- bla.py
+++ bla.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 main_window=Tk()
-main_window.geometry('260x125')
+main_window.geometry('240x200')
 #main_window.config(bg='lig')
 main_window.title('HOMEPAGE')
 
@@ -36,33 +36,36 @@ while(1):
 cursor.execute("use test")
 def INSERT():
     global new_window,ent1,ent2,ent3,ent5,ent6,clicked1,clicked
-    new_window=Toplevel(main_window)
+    #new_window=Toplevel(main_window)
+    new_window=Tk()
     new_window.title("ENTER RECORDS")
     #new_window.config(background='')
     lab1=Label(new_window,text='ADMN NUMBER',font=("times new roman", 13)).grid(row=1,column=0)
     lab2=Label(new_window,text='NAME',font=("times new roman", 13)).grid(row=2,column=0)
     lab3=Label(new_window,text='AGE',font=("times new roman", 13)).grid(row=3,column=0)
     lab4=Label(new_window,text='GENDER',font=("times new roman", 13)).grid(row=4,column=0)
-    lab5=Label(new_window,text='HEIGHT(in cms)',font=("times new roman", 13)).grid(row=5,column=0)
-    lab6=Label(new_window,text='WEIGHT(in kg)',font=("times new roman", 13)).grid(row=6,column=0)
-    lab7=Label(new_window,text='BLOOD GROUP',font=("times new roman", 13)).grid(row=7,column=0)
-    lab8=Label(new_window,text='COVID STATUS',font=("times new roman", 13)).grid(row=8,column=0)
-    lab9=Label(new_window,text='COVID VACCINATION STATUS',font=("times new roman", 13)).grid(row=9,column=0)
-    lab10=Label(new_window,text='PAST HISTORY',font=("times new roman", 13)).grid(row=10,column=0)
-    lab11=Label(new_window,text='').grid(row=11,column=0)
-    lab12=Label(new_window,text='Major illness/operations',font=("times new roman", 13)).grid(row=12,column=0,padx=100)
+    lab5=Label(new_window,text='DOB',font=('times new roman',13)).grid(row=5,column=0)
+    lab6=Label(new_window,text='HEIGHT(in cms)',font=("times new roman", 13)).grid(row=6,column=0)
+    lab7=Label(new_window,text='WEIGHT(in kg)',font=("times new roman", 13)).grid(row=7,column=0)
+    lab8=Label(new_window,text='BLOOD GROUP',font=("times new roman", 13)).grid(row=8,column=0)
+    lab9=Label(new_window,text='COVID STATUS',font=("times new roman", 13)).grid(row=9,column=0)
+    lab10=Label(new_window,text='COVID VACCINATION STATUS',font=("times new roman", 13)).grid(row=10,column=0)
+    lab11=Label(new_window,text='Past History',font=("times new roman", 13)).grid(row=11,column=0)
+    lab12=Label(new_window,text='').grid(row=12,column=0)
+    lab13=Label(new_window,text='Any major illness/operations in past',font=("times new roman", 12)).grid(row=13,column=0,padx=100)
+    lab14=Label(new_window,text='Using any implant/accessories',font=('times new roman',12)).grid(row=14,column=0)
     ent1=Entry(new_window)
     ent1.grid(row=1,column=1,)
     ent2=Entry(new_window)
     ent2.grid(row=2,column=1)
     ent3=Entry(new_window)
     ent3.grid(row=3,column=1)
-    #ent4=Entry(new_window)
-    #ent4.grid(row=4,column=1)
-    ent5=Entry(new_window)
-    ent5.grid(row=5,column=1)
+    #ent5=Entry(new_window)
+    #ent5.grid(row=5,column=1)
     ent6=Entry(new_window)
     ent6.grid(row=6,column=1)
+    ent7=Entry(new_window)
+    ent7.grid(row=7,column=1)
     #ent7=Entry(new_window)
     #ent7.grid(row=7,column=1)
     #ent8=Entry(new_window)
@@ -71,32 +74,30 @@ def INSERT():
     #ent9.grid(row=9,column=1)
     #ent10=Entry(new_window)
     #ent10.grid(row=10,column=1)
+    ent11=Entry(new_window)
+    ent11.grid(row=13,column=1)
     t1 = Checkbutton(new_window, text="Jaundice", variable=var1, onvalue='Jaundice', offvalue='')
-    t1.grid(sticky='W',row=10,column=1)
+    t1.grid(sticky='W',row=11,column=1)
     t2 = Checkbutton(new_window, text="Allergies", variable=var2, onvalue='Allergies', offvalue='')
-    t2.grid(sticky='W',row=10,column=2)
+    t2.grid(sticky='W',row=11,column=2)
     t3 = Checkbutton(new_window, text="Blood Transfusion", variable=var3, onvalue='Blood Transfusion', offvalue='')
-    t3.grid(sticky='W',row=11,column=1)
-    t4 = Checkbutton(new_window, text="high-cholestrol", variable=var4, onvalue='high-cholestrol', offvalue='')
-    t4.grid(row=12,column=2)
-    t5 = Checkbutton(new_window, text="Asthma-or-lung disease", variable=var5, onvalue='Asthma-or-lung disease', offvalue='')
-    t5.grid(sticky='W',row=14,column=1)
-    t6 = Checkbutton(new_window, text="Thyroid-disease", variable=var6, onvalue='Thyroid-disease', offvalue='')
-    t6.grid(sticky='W',row=14,column=2,)
-    t7 = Checkbutton(new_window, text="Liver-disease", variable=var7, onvalue='Liver-disease', offvalue='')
-    t7.grid(sticky='W',row=16,column=1)
-    t8 = Checkbutton(new_window, text="Dementia", variable=var8, onvalue='Dementia', offvalue='')
-    t8.grid(sticky='W',row=16,column=2)
+    t3.grid(sticky='W',row=12,column=1)
+    t4 = Checkbutton(new_window, text="Dental implant", variable=var4, onvalue='Dental implant', offvalue='')
+    t4.grid(sticky='W',row=14,column=1)
+    t5 = Checkbutton(new_window, text='Braces', variable=var5, onvalue='Braces', offvalue='')
+    t5.grid(sticky='W',row=14,column=2)
+    t6 = Checkbutton(new_window, text="Spectacles", variable=var6, onvalue='Spectacles', offvalue='')
+    t6.grid(sticky='W',row=15,column=1)
     t9 = Checkbutton(new_window, text="NIL", variable=var9, onvalue='NIL', offvalue='')
     YES = Radiobutton(new_window,text="YES",variable=f,value='YES',command=vaccine())
-    YES.grid(row=9,column=1,sticky="W")
+    YES.grid(row=10,column=1,sticky="W")
     NO = Radiobutton(new_window,text="NO",variable=f,value='NO',command=vaccine())
-    NO.grid(row=9,column=2,sticky="W")
-    t9.grid(sticky='W',row=18,column=1)
+    NO.grid(row=10,column=2,sticky="W")
+    t9.grid(sticky='W',row=12,column=2)
     YES = Radiobutton(new_window,text="YES",variable=r,value='YES',command=COVID())
-    YES.grid(row=8,column=1,sticky="W")
+    YES.grid(row=9,column=1,sticky="W")
     NO = Radiobutton(new_window,text="NO",variable=r,value='NO',command=COVID())
-    NO.grid(row=8,column=2,sticky="W")
+    NO.grid(row=9,column=2,sticky="W")
     options = [
     "O -ve",
     "O +ve",
@@ -109,24 +110,44 @@ def INSERT():
     ]
     clicked1 = StringVar()
     clicked1.set('BLOOD GROUP')
-    drop=OptionMenu(new_window,clicked1,*options )
-    drop.grid(row=7,column=1)
+    drop=OptionMenu(new_window,clicked1,*options)
+    drop.grid(row=8,column=1)
     options = [
-    "male",
-    "female",
-    "other"
+    "Male",
+    "Female",
+    "Other"
     ]
     clicked = StringVar()
     clicked.set('M/F/O')
     drop=OptionMenu(new_window,clicked,*options )
     drop.grid(row=4,column=1)
+
+    date=[1,2,3,4,5,6,7,8,9,10,11,12,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+    months=['January','February','March','April','May','June','July','August','September','October',
+    'Novermber','December']
+    year=[]
+    for i in range(1940,2050):
+        year.append(i)
+    
+    clicked3=StringVar()
+    clicked3.set('Month')
+    clicked2=IntVar()
+    clicked2.set('Date')
+    clicked4=IntVar()
+    clicked4.set('Year')
+    drop=OptionMenu(new_window,clicked4,*year)
+    drop.grid(row=5,column=3)
+    drop=OptionMenu(new_window,clicked3,*months)
+    drop.grid(sticky='W',row=5,column=2)
+    drop=OptionMenu(new_window,clicked2,*date)
+    drop.grid(sticky='W',row=5,column=1)
     btn=Button(new_window,text="ENTER",command=savedata,bg='light grey')
-    btn.grid(row=19,column=2)
+    btn.grid(row=20,column=2)
     
     bmbtn=Button(new_window,text='FIND bmi',command=BMI,bg='light grey')
-    bmbtn.grid(row=19,column=1)
+    bmbtn.grid(row=20,column=1)
     btnquit=Button(new_window,text='Exit',command=close,bg='light grey')
-    btnquit.grid(row=19,column=3)
+    btnquit.grid(row=20,column=3)
 
 
 def view():
@@ -139,8 +160,9 @@ def view():
     view.title('Details')
     view.geometry('500x300')
     lab1=Label(view,text='ADMN NUMBER :',font=("times new roman", 13)).grid(row=1,column=0)
-    e1=Entry(view).grid(row=1,column=1)
-    #admn=e1.get()
+    admn=Entry(view)
+    admn.grid(row=1,column=1)
+    
     
     def disp():
         
@@ -182,7 +204,8 @@ def view():
         e8=Label(view,text=covid,font=("times new roman", 13)).grid(row=8,column=1)
         e9=Label(view,text=vaccine,font=("times new roman", 13)).grid(row=9,column=1)
         e10=Label(view,text=past,font=("times new roman", 13)).grid(row=10,column=1)
-    btn9=Button(view,text='VIEW',command=disp,bg='light grey').grid(row=1,column=2)
+    btn9=Button(view,text='VIEW',command=disp,bg='light grey')
+    btn9.grid(row=1,column=2)
     
     
 def vaccine():
@@ -270,9 +293,11 @@ def savedata():
     myLabel.grid(row=12,column=0)
 
 
-btn6=Button(text='ADD NEW RECORD',command=INSERT,bg='light grey').grid(row=1,column=0,padx=10,pady=5,ipadx=50)
-btn8=Button(text='DISPLAY RECORD',command=view,bg='light grey').grid(row=2,column=0,padx=10,pady=5,ipadx=50)
-btn7=Button(text='EXIT',command=close1,bg='light grey').grid(row=3,column=0,pady=5,ipadx=25)
+btn6=Button(text='ADD NEW RECORD',command=INSERT,bg='lavender').grid(row=1,column=0,padx=10,pady=5,ipadx=50)
+btn8=Button(text='DISPLAY RECORD',command=view,bg='lavender').grid(row=2,column=0,padx=10,pady=5,ipadx=50)
+btn8=Button(text='MODIFY RECORD',bg='lavender').grid(row=3,column=0,padx=10,pady=5,ipadx=50)
+btn8=Button(text='DELETE RECORD',command=close,bg='lavender').grid(row=4,column=0,padx=10,pady=5,ipadx=50)
+btn7=Button(text='EXIT',command=close1,bg='light grey').grid(row=5,column=0,pady=5,ipadx=25)
 
 
 
